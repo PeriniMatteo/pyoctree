d6086a8202a65e2d06bfaeb844da63210e3b0d89
PeriniMatteo <perini.matteo@gmail.com>
2018-03-14 09:44:27 +0100

    created two new funtions to add and subtract octrees

--- pyoctree/main.cpp
+++ pyoctree/main.cpp
@@ -10,19 +10,16 @@
 
 void ResetInside(cOctNode &node)
 {
-  // Recursive function used by getNodesFromLabel
-  if (node.isLeafNode()) {
-    node.inside = false;
-  } else {
-    for (unsigned int i=0; i<node.branches.size(); i++) {
-      ResetInside(node.branches[i]);
-    }
+  // Recursive function used to "reset" an octree
+  node.inside = false;
+  for (unsigned int i=0; i<node.branches.size(); i++) {
+    ResetInside(node.branches[i]);
   }
 }
 
 void oct_uniform(cOctNode &node1, cOctNode &node2)
 {
-  
+  // Recursive function used to uniform the structure of two existing octree
   auto branchOffsets = cOctree::getBranchOffset();
   if (node1.isLeafNode() != node2.isLeafNode()){
     
@@ -61,101 +58,75 @@ void oct_uniform(cOctNode &node1, cOctNode &node2)
     }
   }
 }
-/*
-void oct_sum(cOctNode &node)
+
+void oct_sum(cOctNode &node1, cOctNode &node2, cOctNode &node3)
 {
-    // Recursive function used by getNodesFromLabel
-    if (node1.isLeafNode() && node2.isLeafNode()) {
-      if (node1.inside || node2.inside) {
+    // Recursive function used to compute boolean sum
+    if (node1.isLeafNode()) {
+      if (node1.numPolys()!=0 || node2.numPolys()!=0) {
         node3.inside = true;
       }
-    } else if (node1.isLeafNode() != node2.isLeafNode()){
-      //if (node1.branches.size() != 0){
-      //}
-      //if (node1.branches.size() != 0){
-      //}
-      if (node1.branches.size() > node2.branches.size()){
-        vector<double> position(3);
-        for (int i=0; i<node2.NUM_BRANCHES_OCTNODE; i++) {
-            for (int j=0; j<3; j++) {
-                position[j] = node2.position[j] + 0.25*node2.size*branchOffsets[i][j]; }
-            string nid = node2.nid + "-" + NumberToString(i);
-            node2.addNode(node2.level+1,nid,position,0.5*node2.size, (int) 1); 
-        }
-      }
-      if (node1.branches.size() < node2.branches.size()){
-        vector<double> position(3);
-        for (int i=0; i<node1.NUM_BRANCHES_OCTNODE; i++) {
-            for (int j=0; j<3; j++) {
-                position[j] = node1.position[j] + 0.25*node1.size*branchOffsets[i][j]; }
-            string nid = node1.nid + "-" + NumberToString(i);
-            node1.addNode(node1.level+1,nid,position,0.5*node1.size, (int) 1); 
-        }
-      }
     } else {
-      if (node2.branches.size() == node1.branches.size()){
-        for (unsigned int i=0; i<node.branches.size(); i++) {
-          ResetInside(node.branches[i]);
-        }
+      for (unsigned int i=0; i<node1.branches.size(); i++) {
+        oct_sum(node1.branches[i], node2.branches[i], node3.branches[i]);
       }
     }
-}*/
+}
 
-cOctree oct_diff(cOctree &o1, cOctree &o2)
+void oct_diff(cOctNode &node1, cOctNode &node2, cOctNode &node3)
+{
+    // Recursive function used to compute boolean difference
+    if (node1.isLeafNode()) {
+      if (node1.inside != node2.inside) {
+        node3.inside = true;
+      }
+    } else {
+      for (unsigned int i=0; i<node1.branches.size(); i++) {
+        oct_diff(node1.branches[i], node2.branches[i], node3.branches[i]);
+      }
+    }
+}
+
+cOctree oct_reset(cOctree &o1, cOctree &o2)
 {
   cOctree o3 = o1;
   ResetInside(o3.root);
-  //oct_uniform(o1.root, o2.root);
-  /*
-  // Recursive function used by getNodesFromLabel
-  if (node.isLeafNode()) {
-    nodeList.push_back(&node);
-  } else {
-    for (unsigned int i=0; i<node.branches.size(); i++) {
-      findIfLeaf(node.branches[i], nodeList);
-    }
-  }*/
   return o3;
 }
-/*
-void CheckIfNodeIsTheSame(cOctNode &node1, cOctNode &node2, cOctNode &node3)
-{
-  if (node1.level < MAX_OCTREE_LEVELS){
-    // Split node into 8 branches
-    for (int i=0; i<node.NUM_BRANCHES_OCTNODE; i++) {
-      string nid = node.nid + "-" + NumberToString(i);
-      
-    }
-  }
-  // Reallocate date from node to branches
-  for (int i=0; i<node.NUM_BRANCHES_OCTNODE; i++) {
-    if (node.branches[i].numPolys()!=0 && node.branches[i].level < MAX_OCTREE_LEVELS){
-      CheckIfNodeIsTheSame(node.branches[i]);
-    }   
-  }
-}
-*/
+
 int main(){
   
   string name;
   name = "./Examples/sphere.stl";
-  int level = 6;
-  cOctree o1 =  oct_builder (name, level-1, 2);
-  cOctree o2 =  oct_builder (name, level, 2);
+  int level = 7;
+  cOctree o1 =  oct_builder ("./Examples/bool3.stl", level, 2);
+  cOctree o2 =  oct_builder ("./Examples/bool4.stl", level, 2);
   
-  std::cout << "inside node of o1 : " << o1.get_Leafs().size() << std::endl;
-  std::cout << "inside node of o2 : " << o2.get_Leafs().size() << std::endl;
+  std::cout << "Leaf nodes of o1 : " << o1.get_Leafs().size() << std::endl;
+  std::cout << "Leaf nodes of o2 : " << o2.get_Leafs().size() << std::endl;
   //std::cout << NumberToString( 45 ) << std::endl;
-  cOctree oo = oct_diff(o1,o2);
-  
-  std::cout << "inside node of oo : " << oo.get_Inside().size() << std::endl;
 
   oct_uniform(o1.root, o2.root);
-  std::cout << "inside node of o1 : " << o1.get_Leafs().size() << std::endl;
-  std::cout << "inside node of o2 : " << o2.get_Leafs().size() << std::endl;
+  cOctree oo = oct_reset(o1,o2);
+  
+  std::cout << std::endl << "inside nodes of oo : " << oo.get_Inside().size() << std::endl ;
+  std::cout << "Leaf nodes of o1 : " << o1.get_Leafs().size() << std::endl;
+  std::cout << "Leaf nodes of o2 : " << o2.get_Leafs().size() << std::endl;
+  
+  std::cout << "inside nodes of o1 : " << o1.get_Inside().size() << std::endl;
+  std::cout << "inside nodes of o2 : " << o2.get_Inside().size() << std::endl;
+  std::cout << "inside nodes of oo : " << oo.get_Inside().size() << std::endl;
+  
+  oct_diff(o1.root, o2.root, oo.root);
+
+  std::cout << "inside nodes of o1 : " << o1.get_Inside().size() << std::endl;
+  std::cout << "inside nodes of o2 : " << o2.get_Inside().size() << std::endl;
+  std::cout << "inside nodes of oo : " << oo.get_Inside().size() << std::endl;
+  
   //oct_save ( o1, 0, "o_all.vtu" );
   //oct_save ( o1, 1, "o_leaf.vtu" );
-  //oct_save ( o1, 2, "o_inside.vtu" );
-  oct_save ( oo, 2, "o_inside.vtu" );
+  oct_save ( o1, 1, "o1_inside.vtu" );
+  oct_save ( o2, 1, "o2_inside.vtu" );
+  oct_save ( oo, 2, "oo_inside.vtu" );
   return 0;
 }
